d62b9f7ee3cc6462177e08e1933eff6084721796
loremcodes <prasadjunghare2@gmail.com>
2023-03-13 18:23:18 +0530

    added firebase config

---
+++ src/database/firebase.js
@@ -0,0 +1,13 @@
+// database/firebaseDb.js
+import * as firebase from 'firebase';
+const firebaseConfig = {
+    apiKey: "AIzaSyCxc80qXEQZil-Lr1Tz5RGy9MnsSuoryrw",
+    authDomain: "http://fpst-b1603.firebaseapp.com/",
+    databaseURL: "https://reactnativefirebase-00000.firebaseio.com",
+    projectId: "fpst-b1603",
+    storageBucket: "http://fpst-b1603.appspot.com/",
+    messagingSenderId: "988057964382",
+    appId: "1:988057964382:web:fefd40d9f07a9b9fbb49cf"
+};
+firebase.initializeApp(firebaseConfig);
+export default firebase;F
